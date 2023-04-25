44d1621e899f9ad4e442f69726b4e1ebb5291af8
Jeffrey Qu <jqu@seas.upenn.edu>
2023-04-25 01:06:16 -0400

    code cleanup

--- server/server.js
+++ server/server.js
@@ -16,13 +16,6 @@ app.get('/closest', routes.closest);
 app.get('/todo', routes.todo);
 app.get('/author/:type', routes.author);
 app.get('/random', routes.random);
-app.get('/song/:song_id', routes.song);
-app.get('/album/:album_id', routes.album);
-app.get('/albums', routes.albums);
-app.get('/album_songs/:album_id', routes.album_songs);
-app.get('/top_songs', routes.top_songs);
-app.get('/top_albums', routes.top_albums);
-app.get('/search_songs', routes.search_songs);
 
 app.listen(config.server_port, () => {
   console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
